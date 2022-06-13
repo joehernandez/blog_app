070e894c886a4bbf62f4f993f8589d3b1ff23e69
Joe Hernandez <joe.a.hernandez@live.com>
2022-06-12 19:47:43 -0500

    Completed Section 6

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'jsonapi-serializer'
+gem 'jsom-pagination', '~>0.1.2'
+gem 'jsonapi_errors_handler'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -10,20 +10,67 @@ RSpec.describe ArticlesController, type: :request do
     it 'returns valid JSON' do
       article = create :article
       get '/articles'
-      body = JSON.parse(response.body).deep_symbolize_keys
-      expect(body).to eq(
-        data: [
-          {
-            id: article.id.to_s,
-            type: 'article',
-            attributes: {
-              title: article.title,
-              content: article.content,
-              slug: article.slug
-            }
-          }
-        ]
-      )
+      expect(json_data.length).to eq(1)
+      expected = json_data.first
+      aggregate_failures do
+        expect(expected[:id]).to eq(article.id.to_s)
+        expect(expected[:type]).to eq('article')
+        expect(expected[:attributes]).to eq(
+          title: article.title,
+          content: article.content,
+          slug: article.slug
+        )
+      end
+    end
+
+    it 'returns articles in expected order' do
+      older_article = create(:article, created_at: 1.hour.ago)
+      recent_article = create(:article, slug: 'new slug')
+      get '/articles'
+      ids = json_data.map { |item| item[:id].to_i }
+      expect(ids).to(eq([recent_article.id, older_article.id]))
+    end
+
+    it 'paginates results' do
+      article1 = create(:article)
+      article2 = create(:article, slug: 'slug-two')
+      article3 = create(:article, slug: 'slug-three')
+      get '/articles', params: {page: { number: 2, size: 1 }}
+      expect(json_data.length).to eq(1)
+      expect(json_data.first[:id]).to eq(article2.id.to_s)
+    end
+
+    it 'contains pagination links' do
+      article1 = create(:article)
+      article2 = create(:article, slug: 'slug-two')
+      article3 = create(:article, slug: 'slug-three')
+      get '/articles', params: {page: { number: 2, size: 1 }}
+      expect(json[:links].length).to eq(5)
+      expect(json[:links].keys).to contain_exactly(:first, :prev, :next, :last, :self)
+    end
+  end
+
+  describe '#show' do
+    let(:article) { create :article }
+
+    subject { get "/articles/#{article.id}" }
+
+    before { subject }
+
+    it 'returns a success response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a proper JSON' do
+      aggregate_failures do
+        expect(json_data[:id]).to eq(article.id.to_s)
+        expect(json_data[:type]).to eq('article')
+        expect(json_data[:attributes]).to eq(
+          title: article.title,
+          content: article.content,
+          slug: article.slug
+        )
+      end
     end
   end
 end
